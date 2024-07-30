09809882c1fa16fcc83eb19ee851a13fbac52ae3
Renato Cavalcanti <renato@cavalcanti.be>
2024-07-31 00:04:36 +0200

    chore: update runtime version to 1.1.38-70edf5a

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val ProtocolVersionMinor = 1
     val RuntimeImage = "gcr.io/kalix-public/kalix-runtime"
     // Remember to bump kalix-runtime.version in maven-java/kalix-spring-boot-parent if bumping this
-    val RuntimeVersion = sys.props.getOrElse("kalix-runtime.version", "1.1.38-8dafb2d")
+    val RuntimeVersion = sys.props.getOrElse("kalix-runtime.version", "1.1.38-70edf5a")
   }
   // NOTE: embedded SDK should have the AkkaVersion aligned, when updating RuntimeVersion, make sure to check
   // if AkkaVersion and AkkaHttpVersion are aligned
