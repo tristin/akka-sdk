cc770825d6e2140990333f7f540969c85f9dc779
Francisco Lopez-Sancho <franciscolopezsancho@gmail.com>
2024-08-30 15:08:27 +0200

    fix: publish milestone to release instead snapshot
    * fix: publish milestone to release instead snapshot

--- .github/publish-maven-to-cloudsmith.sh
+++ .github/publish-maven-to-cloudsmith.sh
@@ -26,25 +26,17 @@ cat <<EOF >~/.m2/settings.xml
 <settings xmlns="http://maven.apache.org/SETTINGS/1.1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
   xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.1.0 http://maven.apache.org/xsd/settings-1.1.0.xsd">
   <servers>
-      <server>
-        <id>cloudsmith-lightbend</id>
+    <server>
+        <id>akka-repo</id>
         <username>${CLOUDSMITH_MACHINE_USER}</username>
         <password>${CLOUDSMITH_MACHINE_PASSWORD}</password>
     </server>
+     <server>
+         <id>akka-snapshot-repo</id>
+         <username>${CLOUDSMITH_MACHINE_USER}</username>
+         <password>${CLOUDSMITH_MACHINE_PASSWORD}</password>
+     </server>
   </servers>
-  <profiles>
-    <profile>
-      <id>cloudsmith-snapshots</id>
-      <activation>
-        <activeByDefault>true</activeByDefault>
-      </activation>
-      <properties>
-        <altSnapshotDeploymentRepository>cloudsmith-lightbend::default::https://maven.cloudsmith.io/lightbend/akka-snapshots/</altSnapshotDeploymentRepository>
-        <altReleaseDeploymentRepository>cloudsmith-lightbend::default::https://maven.cloudsmith.io/lightbend/akka/</altReleaseDeploymentRepository>
-        <altDeploymentRepository>cloudsmith-lightbend::default::https://maven.cloudsmith.io/lightbend/akka-snapshots/</altDeploymentRepository>
-      </properties>
-    </profile>
-  </profiles>
 </settings>
 EOF
 
