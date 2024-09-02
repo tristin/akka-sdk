513d9c55955d508e8e90809c897a3a0091d11e60
Renato Cavalcanti <renato@cavalcanti.be>
2024-09-02 09:55:15 +0200

    chore: temporarily remove service file check

--- samples/java-eventsourced-customer-registry-subscriber/start-upstream.sh
+++ samples/java-eventsourced-customer-registry-subscriber/start-upstream.sh
@@ -4,6 +4,6 @@
 mvn -f ../java-eventsourced-customer-registry/pom.xml compile exec:java --no-transfer-progress &
 
 sleep 7
-echo "Waiting for service to be listed for local discovery"
-while [ ! -f ~/.kalix/local/customer-registry.conf ]; do sleep 1; done
-echo "Listing file present in ~/.kalix/local/customer-registry.conf (until JVM process shuts down)"
+# echo "Waiting for service to be listed for local discovery"
+# while [ ! -f ~/.akka/local/customer-registry.conf ]; do sleep 1; done
+# echo "Listing file present in ~/.akka/local/customer-registry.conf (until JVM process shuts down)"
