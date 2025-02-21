3d535def12a70259deaa8f8e5fc30072c32fd8b7
Andrzej Ludwikowski <andrzej.ludwikowski@lightbend.com>
2025-02-21 12:24:40 +0100

    chore: bump akka version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
   // NOTE: embedded SDK should have the AkkaVersion aligned, when updating RuntimeVersion, make sure to check
   // if AkkaVersion and AkkaHttpVersion are aligned
   // for prod code, they are marked as Provided, but testkit still requires the alignment
-  val AkkaVersion = "2.10.1"
+  val AkkaVersion = "2.10.2"
   val AkkaHttpVersion = "10.7.0" // Note: should at least the Akka HTTP version required by Akka gRPC
 
   // Note: the Scala version must be aligned with the runtime
