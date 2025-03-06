e298db9fb875dfc8cc3a3f805d20d94efa59b363
Patrik Nordwall <patrik.nordwall@gmail.com>
2025-03-06 15:52:23 +0100

    chore: Cleanup key-value-shopping-cart sample (#281)
    * chore: Cleanup key-value-shopping-cart sample
    
    * We are not using DTOs anywhere else between endpoint and entity
    
    * some more DTO

--- samples/key-value-shopping-cart/src/main/java/com/example/domain/ShoppingCart.java
+++ samples/key-value-shopping-cart/src/main/java/com/example/domain/ShoppingCart.java
@@ -1,5 +1,6 @@
 package com.example.domain;
 
+import java.time.Instant;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -7,7 +8,7 @@ import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public record ShoppingCart(String cartId, List<LineItem> items, long creationTimestamp) {
+public record ShoppingCart(String cartId, List<LineItem> items, Instant creationTimestamp) {
 
   public record LineItem(String productId, String name, int quantity) {
     public LineItem withQuantity(int quantity) {
@@ -16,9 +17,9 @@ public record ShoppingCart(String cartId, List<LineItem> items, long creationTim
   }
 
   public ShoppingCart withItem(LineItem itemAdded) {
-    var lineItem = updateItem(itemAdded, this); // <1>
+    var lineItem = updateItem(itemAdded); // <1>
     List<LineItem> lineItems =
-      removeItemByProductId(this, itemAdded.productId()); // <2>
+      removeItemByProductId(itemAdded.productId()); // <2>
     lineItems.add(lineItem); // <3>
     lineItems.sort(Comparator.comparing(LineItem::productId));
     return new ShoppingCart(cartId, lineItems, creationTimestamp); // <4>
@@ -26,23 +27,23 @@ public record ShoppingCart(String cartId, List<LineItem> items, long creationTim
 
   public ShoppingCart withoutItem(LineItem itemRemoved) {
     List<LineItem> updatedItems =
-      removeItemByProductId(this, itemRemoved.productId());
+      removeItemByProductId(itemRemoved.productId());
     updatedItems.sort(Comparator.comparing(LineItem::productId));
     return new ShoppingCart(cartId, updatedItems, creationTimestamp);
   }
 
-  public ShoppingCart withCreationTimestamp(long ts) {
+  public ShoppingCart withCreationTimestamp(Instant ts) {
     return new ShoppingCart(cartId, items, ts);
   }
 
-  private static List<LineItem> removeItemByProductId(ShoppingCart cart, String productId) {
-    return cart.items().stream()
+  private List<LineItem> removeItemByProductId(String productId) {
+    return items().stream()
       .filter(lineItem -> !lineItem.productId().equals(productId))
       .collect(Collectors.toList());
   }
 
-  private static LineItem updateItem(LineItem item, ShoppingCart cart) {
-    return cart.findItemByProductId(item.productId())
+  private LineItem updateItem(LineItem item) {
+    return findItemByProductId(item.productId())
       .map(li -> li.withQuantity(li.quantity() + item.quantity()))
       .orElse(item);
   }
@@ -54,6 +55,6 @@ public record ShoppingCart(String cartId, List<LineItem> items, long creationTim
   }
 
   public static ShoppingCart of(String id) {
-    return new ShoppingCart(id, Collections.emptyList(), 0L);
+    return new ShoppingCart(id, Collections.emptyList(), Instant.EPOCH);
   }
 }
