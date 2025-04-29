56f19796343846221980a86af53cb1ab6e843a83
Patrik Nordwall <patrik.nordwall@gmail.com>
2025-04-29 16:44:28 +0200

    chore: Don't update ask-akka-agent from script (#411)
    * chore: Don't update ask-akka-agent from script
    
    * run script with sudo since antora is creating directories/files as root

--- docs/bin/docs2markdown.sh
+++ docs/bin/docs2markdown.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # Run `make prod` first
 
+# abort script if a command fails
+set -e
+
 find target/site -type d -name support -prune -o -type d -name reference -prune -o -type d -name _attachments -prune -o -type d -name services -prune -o -type d -name libraries -prune -o -name "404.html" -prune -o -name "*.adoc\[\].html" -prune -o \
   -type f -name "*.html" -exec sh -c 'npx -y d2m@latest -e -i {} -o {}.md ' \;
 
@@ -8,4 +11,4 @@ find target/site -type d -name support -prune -o -type d -name reference -prune 
 find target/site -type f -name "*.html.md" -print0 | xargs -0 sed -i '' -E 's/\(([0-9]+)\)/\/\/ (\1)/g'
 
 # update ask-akka-agent
-find target/site -name "index.html.md" -prune -o -type f -name "*.html.md" -exec cp {} samples/ask-akka-agent/src/main/resources/md-docs/ \;
+#find target/site -name "index.html.md" -prune -o -type f -name "*.html.md" -exec cp {} samples/ask-akka-agent/src/main/resources/md-docs/ \;
